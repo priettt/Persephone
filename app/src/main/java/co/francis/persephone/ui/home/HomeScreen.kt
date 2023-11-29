329eef73d71aed69b88a57f9b73f02ae98ecb2a8
Francisco Prieto <priettt@gmail.com>
2023-11-29 17:56:22 -0300

    Created PersephoneApp.kt & HomeScreen.kt

---
+++ app/src/main/java/co/francis/persephone/ui/home/HomeScreen.kt
@@ -0,0 +1,23 @@
+package co.francis.persephone.ui.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
