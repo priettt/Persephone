d17f5a85a98e39d1878a5e4e4a1e1ec44a929735
Francisco Prieto <priettt@gmail.com>
2023-11-30 00:19:43 -0300

    Rounded images and added home screen title

--- app/src/main/java/co/francis/persephone/ui/home/HomeScreen.kt
+++ app/src/main/java/co/francis/persephone/ui/home/HomeScreen.kt
@@ -2,27 +2,42 @@ package co.francis.persephone.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import co.francis.persephone.R
 
 @Composable
 fun HomeScreen() {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(3),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        items(20) {
-            Image(
-                painter = painterResource(id = R.drawable.plantita),
-                contentDescription = "A test plant"
-            )
+    Column {
+        Text(
+            text = "Home",
+            fontSize = 32.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(20) {
+                Image(
+                    modifier = Modifier.clip(RoundedCornerShape(20.dp)),
+                    painter = painterResource(id = R.drawable.plantita),
+                    contentDescription = "A test plant"
+                )
+            }
         }
     }
 }
