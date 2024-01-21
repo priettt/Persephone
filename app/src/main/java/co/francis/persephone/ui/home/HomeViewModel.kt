06c06b35124ad11b83857b69776c171d39377df3
Francisco Prieto <priettt@gmail.com>
2024-01-21 19:47:06 -0300

    Plants repository now exposes a flow
    Added plants after a short delay to verify that they get added to the HomeScreen

--- app/src/main/java/co/francis/persephone/ui/home/HomeViewModel.kt
+++ app/src/main/java/co/francis/persephone/ui/home/HomeViewModel.kt
@@ -1,15 +1,25 @@
 package co.francis.persephone.ui.home
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(
     private val plantsRepository: PlantsRepository = PlantsRepository()
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(HomeUiState(plantsRepository.getPlants()))
-    val uiState = _uiState.asStateFlow()
+    val uiState: StateFlow<HomeUiState> =
+        plantsRepository
+            .observePlants()
+            .map { plants -> HomeUiState(plants) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = HomeUiState(emptyList())
+            )
 
 }
 
--- app/src/main/java/co/francis/persephone/ui/home/PlantsRepository.kt
+++ app/src/main/java/co/francis/persephone/ui/home/PlantsRepository.kt
@@ -1,20 +1,43 @@
 package co.francis.persephone.ui.home
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import java.util.Timer
+import kotlin.concurrent.schedule
+
 class PlantsRepository {
-    fun getPlants(): List<Plant> {
-        return listOf(
+
+    init {
+        Timer().schedule(5000) {
+            plants.update {
+                it + Plant("Bonsai") + Plant("Orquidea") + Plant("Calla")
+            }
+        }
+    }
+
+    private val plants = MutableStateFlow(
+        listOf(
             Plant("Potus"),
             Plant("Dieffenbachia"),
             Plant("Gomero"),
             Plant("Cactus"),
-            Plant("Bonsai"),
-            Plant("Orquidea"),
-            Plant("Calla"),
-            Plant("Crisantemo"),
-            Plant("Lirio"),
-            Plant("Tulipan"),
-            Plant("Rosa"),
         )
-    }
+    )
+
+    fun observePlants(): Flow<List<Plant>> = plants
+
+//            Plant("Potus"),
+//            Plant("Dieffenbachia"),
+//            Plant("Gomero"),
+//            Plant("Cactus"),
+//            Plant("Bonsai"),
+//            Plant("Orquidea"),
+//            Plant("Calla"),
+//            Plant("Crisantemo"),
+//            Plant("Lirio"),
+//            Plant("Tulipan"),
+//            Plant("Rosa"),
 }
+
 
