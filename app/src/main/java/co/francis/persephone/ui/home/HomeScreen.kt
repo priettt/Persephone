6c9e5f83e0e607029470a542d879473f6ea61c8f
Francisco Prieto <priettt@gmail.com>
2024-01-23 00:13:42 -0300

    Added a scaffold and a FAB to the home screen

--- app/src/main/java/co/francis/persephone/ui/home/HomeScreen.kt
+++ app/src/main/java/co/francis/persephone/ui/home/HomeScreen.kt
@@ -9,12 +9,18 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -25,21 +31,34 @@ fun HomeScreen(
     homeViewModel: HomeViewModel = viewModel()
 ) {
     val uiState = homeViewModel.uiState.collectAsStateWithLifecycle()
-    Column {
-        Text(
-            text = "Home",
-            fontSize = 40.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(16.dp)
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                content = { Icon(Icons.Default.Add, "Add a plant") }
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
         ) {
-            val plants: List<Plant> = uiState.value.plants
-            items(plants) {
-                PlantCard(plant = it)
+            Text(
+                text = "My plants",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+            )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                val plants: List<Plant> = uiState.value.plants
+                items(plants) {
+                    PlantCard(plant = it)
+                }
             }
         }
     }
