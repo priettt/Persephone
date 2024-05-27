040e8db98f314b5a8c5fcdede570f49feec3fad3
Francisco Prieto <priettt@gmail.com>
2024-05-27 15:30:17 -0300

    added embrace

--- app/src/main/java/co/francis/persephone/MainActivity.kt
+++ app/src/main/java/co/francis/persephone/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import co.francis.persephone.ui.navigation.PersephoneNavGraph
+import io.embrace.android.embracesdk.Embrace
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Embrace.getInstance().start(this)
         setContent { PersephoneNavGraph() }
     }
 }
